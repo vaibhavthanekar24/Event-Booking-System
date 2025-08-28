d19aab048d8aa10e719f1c3a4a4ad50a3cd595bc
vaibhavthanekar24 <vaibhavthanekar007@gmail.com>
2025-08-28 23:00:00 +0530

    new changes added

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,37 +1,16 @@
-import { createNextPayloadHandler } from '@payloadcms/next'
-import payload from '../../payload'
+import {
+  REST_DELETE,
+  REST_GET,
+  REST_OPTIONS,
+  REST_PATCH,
+  REST_POST,
+  REST_PUT,
+} from '@payloadcms/next/routes'
+import { getPayload } from '../../payload'
 
-export const GET = createNextPayloadHandler({
-  payload,
-  options: {
-    generateNextApiEndpoint: true,
-  },
-})
-
-export const POST = createNextPayloadHandler({
-  payload,
-  options: {
-    generateNextApiEndpoint: true,
-  },
-})
-
-export const PUT = createNextPayloadHandler({
-  payload,
-  options: {
-    generateNextApiEndpoint: true,
-  },
-})
-
-export const PATCH = createNextPayloadHandler({
-  payload,
-  options: {
-    generateNextApiEndpoint: true,
-  },
-})
-
-export const DELETE = createNextPayloadHandler({
-  payload,
-  options: {
-    generateNextApiEndpoint: true,
-  },
-})
+export const GET = REST_GET({ getPayload })
+export const POST = REST_POST({ getPayload })
+export const PUT = REST_PUT({ getPayload })
+export const PATCH = REST_PATCH({ getPayload })
+export const DELETE = REST_DELETE({ getPayload })
+export const OPTIONS = REST_OPTIONS({ getPayload })

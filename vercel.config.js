b995229c392766a20d792da7b45b31f5bff4ac51
vaibhavthanekar24 <vaibhavthanekar007@gmail.com>
2025-08-28 19:37:17 +0530

    added secrets for deployment

--- vercel.config.js
+++ vercel.config.js
@@ -1,16 +1,17 @@
 module.exports = {
   // Specify the regions where your code should be deployed
   regions: ['iad1'],
-  
+
   // Configure the build output directory
   outputDirectory: '.next',
-  
+
   // Configure build environment variables
   build: {
     env: {
-      PAYLOAD_SECRET: '@payload_secret',
-      DATABASE_URI: '@database_uri',
-      NEXT_PUBLIC_SERVER_URL: '@next_public_server_url'
-    }
-  }
+      PAYLOAD_SECRET: '3b59baf6902a880d643ee5d9',
+      DATABASE_URI:
+        'mongodb+srv://vaibhavthanekar007_db_user:2Y8dXxMwC9Sy07np@clusterforpayloadcms.oruemqm.mongodb.net/',
+      NEXT_PUBLIC_SERVER_URL: '@next_public_server_url',
+    },
+  },
 }
